40ee9b5b090ba7557a233235e20ca153b9d34eec
Lucas Natan de Almeida Melo <lucasnatan.dev@gmail.com>
2022-05-20 16:36:31 -0400

    Included click buttons feature

--- script.js
+++ script.js
@@ -7,6 +7,19 @@ document.body.addEventListener('keyup', (e)=>{
     playSound(e.code.toLowerCase() ); //Função playSound irá tocar o comando recebido
 });                                //toLouwerCase irá sempre diminuir o caractere
 
+//Função para adicionar função de click nos botões
+document.querySelectorAll('.key').forEach(item => {
+    item.addEventListener('click', itemClick)
+}); //Irá percorrer em cada item e irá adicionar um evento de clique em cada um
+    //Veja que é utilizado o querySelectorAll o qual irá filtar todos com a tag .item
+
+
+function itemClick(e){ //Função que irá informar o item selecionado 
+    let item = e.target.getAttribute('data-key'); //Armazena em item a informação da div clickada (Utiliza o target) 
+    console.log(item);
+    playSound(item); //Veja que não foi necessário pegar o código da tecla nem colocá-la em minúsculo
+}
+
 
 //Essa função irá pegar os caracteres informados no input e colocar em uma lista denominada songArray
 document.querySelector('.composer button').addEventListener('click', ()=> {
@@ -23,7 +36,7 @@ function playSound(sound){
                                                               //O parametro sound pode ser qualquer coisa, no caso será o caractere pressionado
 
     let keyElement = document.querySelector(`div[data-key="${sound}"]`) //Aqui será definido a partir do caractere digitado para manipular a div data-key
-    
+    console.log(keyElement);
     
     if(audioElement){        //Se o audio Element for encontrado... Veja que não necessitou de condicional - Já filtra por padrão os caracteres
         audioElement.currentTime = 0; //Comando para sempre que ser pressionada a tecla, ser zerado contador do áudio para evitar bug
